a75b15929b58450c9c63a5f4ad20a1416bed3ff9
Sumanadithan <sumanadithan34@gmail.com>
2023-02-23 20:59:12 +0530

    fourth commit

--- index.js
+++ index.js
@@ -1,36 +1,44 @@
-let navLinks = document.querySelector('#navLinks');
+let navLinks = document.querySelector("#navLinks");
 
-function showMenu(){
-	navLinks.style.right = '0';
+function showMenu() {
+	navLinks.style.right = "0";
 }
 
-function hideMenu(){
-	navLinks.style.right = '-200px';
+function hideMenu() {
+	navLinks.style.right = "-200px";
 }
 
-let contents = ['eventContent','registerContent','commiteeContent'];
+let contents = ["eventContent", "registerContent", "commiteeContent"];
+let contentHeadings = ["eh", "rh", "ch"];
+let contentLabels = ["el", "rl", "cl"];
 let visibleContentId = null;
 
-function toggleVisibility(contentId){
-	console.log(contentId);
-	if(visibleContentId === contentId){
-		// visibleDivId = null;
+function toggleVisibility(contentId) {
+	if (visibleContentId === contentId) {
+		visibleDivId = null;
 	} else {
-		visibleContentId = contentId
+		visibleContentId = contentId;
 	}
 	hideNonVisibleContent();
 }
 
-function hideNonVisibleContent(){
-	console.log('hi')
-	let i,cId,content;
-	for(i=0;i<contents.length;i++){
+function hideNonVisibleContent() {
+	let i, cId, content, cHeading, cLabel;
+	for (i = 0; i < contents.length; i++) {
 		cId = contents[i];
+		cHeading = contentHeadings[i];
+		cLabel = contentLabels[i];
+		ch = document.getElementById(cHeading);
+		cl = document.getElementById(cLabel);
 		content = document.getElementById(cId);
-		if(visibleContentId === cId){
-			content.classList.remove('check');
-			console.log('hihi')
+		if (visibleContentId === cId) {
+			ch.style.color = "#f5a425";
+			cl.style.border = "2px solid #f5a425";
+			content.classList.remove("check");
 		} else {
-			content.classList.add('check');		}
+			cl.style.border = "none";
+			content.classList.add("check");
+			ch.style.color = "#fff";
+		}
 	}
 }
