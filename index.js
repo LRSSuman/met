3c6cf4f61411a6b5a3ef1ad0fded3a4aa18dce6d
Sumanadithan <sumanadithan34@gmail.com>
2023-02-23 19:47:12 +0530

    third commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
- var navLinks = document.querySelector('#navLinks');
+let navLinks = document.querySelector('#navLinks');
 
 function showMenu(){
 	navLinks.style.right = '0';
@@ -8,3 +8,29 @@ function hideMenu(){
 	navLinks.style.right = '-200px';
 }
 
+let contents = ['eventContent','registerContent','commiteeContent'];
+let visibleContentId = null;
+
+function toggleVisibility(contentId){
+	console.log(contentId);
+	if(visibleContentId === contentId){
+		// visibleDivId = null;
+	} else {
+		visibleContentId = contentId
+	}
+	hideNonVisibleContent();
+}
+
+function hideNonVisibleContent(){
+	console.log('hi')
+	let i,cId,content;
+	for(i=0;i<contents.length;i++){
+		cId = contents[i];
+		content = document.getElementById(cId);
+		if(visibleContentId === cId){
+			content.classList.remove('check');
+			console.log('hihi')
+		} else {
+			content.classList.add('check');		}
+	}
+}
