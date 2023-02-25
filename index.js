b8fb5d902aa3aa4b44ccd8ea831f0ae8a204390b
Sumanadithan <sumanadithan34@gmail.com>
2023-02-25 17:56:10 +0530

    7th commit

--- index.js
+++ index.js
@@ -42,3 +42,25 @@ function hideNonVisibleContent() {
 		}
 	}
 }
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  spaceBetween: 200,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },  
+});
